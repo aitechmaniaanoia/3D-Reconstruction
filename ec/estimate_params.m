b0b0b54d08c75b5498c5553e8c687469c19eafc6
zoe <wang1993x@gmail.com>
2020-03-01 19:45:48 -0800

    mar 1

--- ec/estimate_params.m
+++ ec/estimate_params.m
@@ -1,3 +1,37 @@
 function [K, R, t] = estimate_params(P)
 % ESTIMATE_PARAMS computes the intrinsic K, rotation R and translation t from
 % given camera matrix P.
+[~,~,V] = svd(P);
+c = V(:,end);
+%c = c/c(4); % 4*1
+
+K = zeros(size(P));
+R = zeros(size(P,2),size(P,2));
+
+% QR decomposition
+for i = 1:size(P,1)
+    K(i,1) = P(i,1)/norm(P(:,1));
+end
+
+for j = 2:size(P,2)
+   sum = zeros(size(P,1),1); 
+   for k = 1:j-1
+       sum = sum + (P(:,j)'*K(:,k))*K(:,k);
+   end
+   z = P(:,i)-sum;
+   
+   for s = 1:size(P,1)
+       K(s,j) = z(s)/norm(z);
+   end
+end
+
+% calculate R
+for i = 1:size(P,1)
+    for j = i:size(P,2)
+        R(i,j) = K(:,i)'*P(:,j);
+    end
+end
+
+% calculate t
+t = -R*c;
+end
--- ec/estimate_pose.m
+++ ec/estimate_pose.m
@@ -1,6 +1,25 @@
 function P = estimate_pose(x, X)
-% ESTIMATE_POSE computes the pose matrix (camera matrix) P given 2D and 3D
-% points.
-%   Args:
-%       x: 2D points with shape [2, N]
-%       X: 3D points with shape [3, N]
+% x : 2*N (x,y) points in image
+% X: 3*N (x,y,z) 3d points
+
+n = size(x,2);
+A = [];
+b = [];
+
+for i = 1:n
+    pt_3d = X(:,i);
+    pt_2d = x(:,i);
+    
+    a1 = [pt_3d(1) pt_3d(2) pt_3d(3) 1 0 0 0 0 -pt_2d(1)*pt_3d(1) -pt_2d(1)*pt_3d(2) -pt_2d(1)*pt_3d(3)]; 
+    a2 = [0 0 0 0 pt_3d(1) pt_3d(2) pt_3d(3) 1 -pt_2d(2)*pt_3d(1) -pt_2d(2)*pt_3d(2) -pt_2d(2)*pt_3d(3)];
+    
+    A = [A; a1; a2];
+    
+    b = [b; pt_2d(1); pt_2d(1)];
+end
+
+P = A\b;
+P = [P;1];
+P = reshape(P,[],3)';
+
+end
