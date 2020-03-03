5598a4286ae5862c62815d84720f2927689dc3d3
zoe <wang1993x@gmail.com>
2020-03-02 21:52:24 -0800

    mar 2

--- ec/estimate_params.m
+++ ec/estimate_params.m
@@ -1,37 +1,58 @@
 function [K, R, t] = estimate_params(P)
 % ESTIMATE_PARAMS computes the intrinsic K, rotation R and translation t from
 % given camera matrix P.
-[~,~,V] = svd(P);
-c = V(:,end);
-%c = c/c(4); % 4*1
+P = P/P(3,4);
 
-K = zeros(size(P));
-R = zeros(size(P,2),size(P,2));
+u = dot(P(1,1:3),P(3,1:3)')./(norm(P(3,1:3))^2);
+v = dot(P(3,1:3),P(2,1:3)')./(norm(P(3,1:3))^2);
 
-% QR decomposition
-for i = 1:size(P,1)
-    K(i,1) = P(i,1)/norm(P(:,1));
-end
+alpha_u = norm(cross(P(1,1:3)',P(3,1:3)'))./(norm(P(3,1:3))^2);
+alpha_v = norm(cross(P(3,1:3)',P(2,1:3)'))./(norm(P(3,1:3))^2);
 
-for j = 2:size(P,2)
-   sum = zeros(size(P,1),1); 
-   for k = 1:j-1
-       sum = sum + (P(:,j)'*K(:,k))*K(:,k);
-   end
-   z = P(:,i)-sum;
-   
-   for s = 1:size(P,1)
-       K(s,j) = z(s)/norm(z);
-   end
-end
+tz = 1./norm(P(3,1:3));
 
-% calculate R
-for i = 1:size(P,1)
-    for j = i:size(P,2)
-        R(i,j) = K(:,i)'*P(:,j);
-    end
-end
+% define the calibration matrix
+K = [alpha_u 0 u;
+     0 alpha_v v;
+     0 0 1];
+ 
+% define extrinsic matrix
+%RT=inv(K)*P*tz;
+Rt = K\P*tz;
+t = Rt(:,4);
+R = Rt(:,1:3);
 
-% calculate t
-t = -R*c;
+% [~,~,V] = svd(P);
+% c = V(:,end);
+% %c = c/c(4); % 4*1
+% 
+% K = zeros(size(P));
+% R = zeros(size(P,2),size(P,2));
+% 
+% % QR decomposition
+% for i = 1:size(P,1)
+%     K(i,1) = P(i,1)/norm(P(:,1));
+% end
+% 
+% for j = 2:size(P,2)
+%    sum = zeros(size(P,1),1); 
+%    for k = 1:j-1
+%        sum = sum + (P(:,j)'*K(:,k))*K(:,k);
+%    end
+%    z = P(:,i)-sum;
+%    
+%    for s = 1:size(P,1)
+%        K(s,j) = z(s)/norm(z);
+%    end
+% end
+% 
+% % calculate R
+% for i = 1:size(P,1)
+%     for j = i:size(P,2)
+%         R(i,j) = K(:,i)'*P(:,j);
+%     end
+% end
+% 
+% % calculate t
+% t = -R*c;
 end
--- ec/projectCAD.m
+++ ec/projectCAD.m
@@ -9,18 +9,26 @@ n = size(pnp.X, 2); % 3*N
 % Use P to project the X onto the image
 X = [pnp.X ; zeros(1,n)];  %4*N
 p_proj = P*X;
-p_proj = p_proj/p_proj(3,:);
+%p_proj = p_proj./p_proj(3,:);
 
 % Plot x and the projected 3D points on screen (plot)
-x = [pnp.x ; ones(1,n)]';
+%x = [pnp.x ; ones(1,n)]';
 imshow(pnp.image,[]); hold on;
-plot(round(x), 'r.');
-xlabel({'plot given x and projected 3D points'})
+plot(pnp.x(1,:),pnp.x(2,:), 'r.');
+plot(p_proj(2,:), p_proj(1,:), 'b*');
+xlabel({'Plot given x and projected 3D points'})
 
 % Draw the CAD model rotated by your estimated rotation R on screen (trimesh)
+%face = R*pnp.cad.faces';
+face = R*pnp.cad.vertices';
+%[x_,y_] = meshgrid(X(1,:),X(2,:));
+tri = delaunay(face(1,:),face(2,:));
+%z = face(:,3);
+trimesh(tri,face(1,:),face(2,:),face(3,:));
 
 % Project the CAD’s all vertices onto the image and draw the projected CAD model 
 % overlapping with the 2D image (patch)
+
 
 
 
