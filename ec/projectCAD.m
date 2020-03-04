5ca058a534d6c7fddf296f13bdc2e75636497e7f
zoe <wang1993x@gmail.com>
2020-03-03 19:25:46 -0800

    mar 3 finally

--- ec/projectCAD.m
+++ ec/projectCAD.m
@@ -15,22 +15,23 @@ p_proj = p_proj(1:2, :);
 
 % Plot x and the projected 3D points on screen (plot)
 
-x = [pnp.x ; ones(1,n)]';
-imshow(pnp.image,[]); hold on;
-xlabel({'Plot given x and projected 3D points'});
-plot(pnp.x(1,:),pnp.x(2,:), 'b.');
-plot(p_proj(1,:), p_proj(2,:), 'yo');
+% x = [pnp.x ; ones(1,n)]';
+% imshow(pnp.image,[]); hold on;
+% xlabel({'Plot given x and projected 3D points'});
+% plot(pnp.x(1,:),pnp.x(2,:), 'b.');
+% plot(p_proj(1,:), p_proj(2,:), 'yo');
 
 % Draw the CAD model rotated by your estimated rotation R on screen (trimesh)
 
-face = pnp.cad.faces;
-vertices = R*pnp.cad.vertices';
-trimesh(face,vertices(1,:),vertices(2,:),vertices(3,:));
+% face = pnp.cad.faces;
+% vertices = R*pnp.cad.vertices';
+% trimesh(face,vertices(1,:),vertices(2,:),vertices(3,:));
 
 % Project the CADs all vertices onto the image and draw the projected CAD model 
 % overlapping with the 2D image (patch)
 
 N = size(pnp.cad.vertices,1);
+%P = P/P(end,end);
 vertices_proj = P*[pnp.cad.vertices ones(N,1)]';
 %vertices_proj = vertices_proj./vertices_proj(3,:);
 vertices_proj(1,:) = vertices_proj(1,:)./vertices_proj(3,:);
